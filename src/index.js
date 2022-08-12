86a4ea4b4be8e2ea0e3a2c13b169fcc85106d128
annashcherenkova <a.shcherenkova@gmail.com>
2022-08-13 00:25:04 +0300

    feat: towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = [];
+  if (Array.isArray(matrix) && (matrix.length>0)) {
+    matrix.forEach((value, index) => {
+      if (index % 2 === 0) {
+        res.push(value);        
+      }
+      else {
+        res.push(value.sort((item1, item2) => item1 > item2 ? -1 : 1))
+      }
+    });      
+    
+    return res.flat();
+  }
+  else {
+    return [];
+  }
 }
